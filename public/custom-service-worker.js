769d15a891e51e97b37d045e0f2921212a7f02af
jesus <jesuslas@gmail.com>
2020-02-21 18:56:34 -0300

    se agrega custom service worker

---
+++ public/custom-service-worker.js
@@ -0,0 +1,35 @@
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js"
+);
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
+
+// workbox.routing.registerRoute(
+//   'http://localhost:8000/todos',
+//   workbox.strategies.staleWhileRevalidate()
+// )
+
+workbox.routing.registerRoute(
+  /\.(?:js|css|html|png)$/,
+  workbox.strategies.networkFirst()
+);
+// workbox.routing.registerRoute(
+//   'http://localhost:3000',
+//   workbox.strategies.networkFirst()
+// )
+
+workbox.routing.registerRoute(
+  new RegExp("https://api.openweathermap.org/"),
+  workbox.strategies.staleWhileRevalidate()
+);
+
+/*
+workbox.routing.registerRoute(
+  new RegExp('https://randomuser.me/api'),
+  workbox.strategies.cacheFirst()
+)
+*/
